14a6a478694ed009c4cf775ce4c5e34762d0d7fa
Priyanka6276 <priyankachanbrasegaran@gmail.com>
2022-11-01 15:31:59 -0400

    first commit: layout for start of game

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const startContainer = document.querySelector(".start-container")
+//===================BEFORE STORY PORTION OF GAME===================
+
+const startContainer = document.querySelector("#start-container") //the container at the start
+const nameContainer  = document.querySelector("#name-container")
+const classesContainer = document.querySelector("#classes-container")
 const startText = document.querySelector("#ready") // "Are you ready" text
 const startButton = document.querySelector("#start-buttton") // the start button at the beginning of the game
 
-startButton.addEventListener("click", () => {
+startButton.addEventListener("click", () => { //when start button is clicked it will lead to enter one's name
     gameStart()
 })
 
 function gameStart () {
     startContainer.classList.add("hide")
+    nameContainer.classList.remove("hide")
+    classesContainer.classList.remove("hide")
 }
+
